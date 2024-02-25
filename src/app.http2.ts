5e49433256244ba1ebe91e7ca67d07f7d3e2d764
fer8614 <fernando.inversionmf@gmail.com>
2024-02-25 09:42:52 -0800

    Deploy cjanges Railway V2

--- src/app.http2.ts
+++ src/app.http2.ts
@@ -8,7 +8,7 @@ const server = http2.createSecureServer({
 },(req, res) => {
 
   console.log(req.url);
-
+   
   // res.writeHead(200, { 'Content-Type': 'text/html' });
   // res.write(`<h1>URL ${ req.url }</h1>`);
   // res.end();
